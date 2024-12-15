569eb27dfaaf6ebd96e1dd6baf32c7e7fcfe4c2a
bluelkh0812 <bluelkh0812@gmail.com>
2024-12-15 15:51:08 +0900

    2024년 12월 15일 추가

---
+++ src/Gugudan.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Gugudan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i = 1;
+
+        int dan = 2;
+
+        while(dan < 10) {
+            i = 1;
+            System.out.printf("%d단", dan);
+            while (i < 10) {
+                System.out.printf("dan x %d = %d\n", dan, i, dan * i);
+                i += 1;
+            }
+
+            dan += 1;
+        }
+
+    }
+}
